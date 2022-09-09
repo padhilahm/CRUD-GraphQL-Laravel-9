e67b26e435833a88ead72e08a605e1e9d6069341
Muhammad Padhilah <32585948+padhilahm@users.noreply.github.com>
2022-09-09 15:11:54 +0800

    Initial commit

---
+++ app/GraphQL/Queries/MovieQuery.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Movie;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class MovieQuery extends Query
+{
+    protected $attributes = [
+        "name" => "movie",
+    ];
+    public function type(): Type
+    {
+        return GraphQL::type("Movie");
+    }
+    public function args(): array
+    {
+        return [
+            "id" => [
+                "name" => "id",
+                "type" => Type::int(),
+                "rules" => ["required"],
+            ],
+        ];
+    }
+    public function resolve($root, $args)
+    {
+        return Movie::findOrFail($args["id"]);
+    }
+}
---
+++ app/GraphQL/Queries/MoviesQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\graphql\Queries;
+
+use App\Models\Movie;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class MoviesQuery extends Query
+{
+    protected $attributes = [
+        "name" => "movies",
+    ];
+    public function type(): Type
+    {
+        return Type::listOf(GraphQL::type("Movie"));
+    }
+    public function resolve($root, $args)
+    {
+        return Movie::all();
+    }
+}
